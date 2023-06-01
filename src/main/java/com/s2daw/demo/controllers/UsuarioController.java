be703033a08f92a06a29d38cabdfa4c65e7e7d05
alumno.411611 <alumno.411611@ies-azarquiel.es>
2023-06-01 10:37:46 +0200

    commit error al tener target y .idea

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,5 +1,7 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +10,46 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="prueba")
-    public List<String> prueba(){
-        return List.of("prueba","prueba2","prueba3");
+    @RequestMapping(value="usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id) {
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Jorge");
+        usuario.setApellidos("Ramirez");
+        usuario.setEmail("jorgeramirezguti@gmail.com");
+        usuario.setTelefono("653915239");
+
+        return usuario;
+    }
+        @RequestMapping(value="usuario567")
+        public Usuario editar() {
+            Usuario usuario = new Usuario();
+            usuario.setNombre("Jorge");
+            usuario.setApellidos("Ramirez");
+            usuario.setEmail("jorgeramirezguti@gmail.com");
+            usuario.setTelefono("653915239");
+
+            return usuario;
+        }
+            @RequestMapping(value="usuario345")
+            public Usuario eliminar() {
+            Usuario usuario = new Usuario();
+            usuario.setNombre("Jorge");
+            usuario.setApellidos("Ramirez");
+            usuario.setEmail("jorgeramirezguti@gmail.com");
+            usuario.setTelefono("653915239");
+
+            return usuario;
+        }
+                @RequestMapping(value="usuario123")
+                public Usuario buscar() {
+                    Usuario usuario = new Usuario();
+                    usuario.setNombre("Jorge");
+                    usuario.setApellidos("Ramirez");
+                    usuario.setEmail("jorgeramirezguti@gmail.com");
+                    usuario.setTelefono("653915239");
+
+                    return usuario;
+
     }
 }
